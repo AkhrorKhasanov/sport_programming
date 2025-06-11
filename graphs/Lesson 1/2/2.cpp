5be9fb5fb39b036934cd2a0c21c7bf924b4e90c9
AkhrorKhasanov <ahror.hasanov.97@mail.ru>
2025-06-11 09:31:55 +0500

    11.06.2025 09:31

---
+++ graphs/Lesson 1/2/2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+using namespace std; 
+int main()
+{
+    int n, m, i;
+    cin >> n >> m;
+    vector <vector <int> > graph(n, vector <int>());
+    for (i = 0; i < m; i++) {
+        int u, v;
+        cin >> u >> v;
+        graph[u].push_back(v);
+        graph[v].push_back(u);
+    }
+    for (i = 0; i < n; i++) {
+        cout << i << " - tugundan " << graph[i].size() << " qirra chiqadi" << endl;
+    }
+    return 0;
+}
---
+++ graphs/Lesson 1/3/3.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+using namespace std; 
+int main()
+{
+    int n, m, i;
+    cin >> n >> m;
+    vector <vector <int> > graph(n, vector <int>());
+    for (i = 0; i < m; i++) {
+        int u, v;
+        cin >> u >> v;
+        graph[u].push_back(v);
+    }
+    vector <int> a(n, 0);
+    for (i = 0; i < n; i++) {
+        for (int j = 0; j < graph[i].size(); j++) {
+            a[graph[i][j]]++;
+        }
+    }
+    for (i = 0; i < n; i++) {
+        cout << i << " - tugunga " << a[i] << " qirra kiradi" << endl;
+    }
+    return 0;
+}
